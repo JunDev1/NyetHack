c15d9a81d63da604d094ec8923b9a70262d65fae
Damdin <damdin.sandanov2@bk.ru>
2023-03-08 15:59:22 +0300

    Chapter 8

--- src/main/kotlin/Narrator.kt
+++ src/main/kotlin/Narrator.kt
@@ -1,9 +1,56 @@
-val narratorModifier : (String) -> String = {
-    val numExclamationPoints = 3
-    it.uppercase()+ "!".repeat(numExclamationPoints)
-}
-fun narrate(
-    message: String
+import kotlin.random.Random
+import kotlin.random.nextInt
+
+var narrationModifier: (String) -> String = { it }
+inline fun narrate(
+    message: String,
+    modifier: (String) -> String = { narrationModifier(it) }
 ) {
-    println(narratorModifier(message))
+//    println(narrationModifier(message))
+    println(modifier(message))
+}
+
+fun changeNarratorMood() {
+    val mood: String
+    val modifier: (String) -> String
+    when (Random.nextInt(1..5)) {
+        1 -> {
+            mood = "loud"
+            modifier = { message ->
+                val numExclamationPoints = 3
+                message.uppercase() + "!".repeat(numExclamationPoints)
+            }
+        }
+
+        2 -> {
+            mood = "tired"
+            modifier = { message ->
+                message.lowercase().replace("", "...")
+            }
+        }
+
+        3 -> {
+            mood = "unsure"
+            modifier = { message ->
+                "$message?"
+            }
+        }
+
+        4 -> {
+            var narratorGiven = 0
+            mood = "like sending an itemized bill"
+            modifier = { message ->
+                narratorGiven++
+                "$message.\n(I have narrated $narratorGiven things)"
+            }
+        }
+        else -> {
+            mood = "professional"
+            modifier = { message ->
+                "$message."
+            }
+        }
+    }
+    narrationModifier = modifier
+    narrate("The narrator begins to feel $mood")
 }
