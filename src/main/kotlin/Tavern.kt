1a8d25b94525e9d41ac0367e2d225240af4681c0
Damdin <damdin.sandanov2@bk.ru>
2023-04-01 09:41:40 +0300

    Chapter 11

--- src/main/kotlin/Tavern.kt
+++ src/main/kotlin/Tavern.kt
@@ -1,4 +1,6 @@
 import java.io.File
+import kotlin.random.Random
+import kotlin.random.nextInt
 
 private const val TAVERN_MASTER = "Taernyl"
 private const val TAVERN_NAME = "$TAVERN_MASTER's Folly"
@@ -8,44 +10,66 @@ private val lastNames = setOf<String>("Ironfoot", "Fernsworth", "Baggins", "Down
 private val menuData = File("src/main/data/tavern-menu-data.txt")
     .readText()
     .split("\n")
-private val menuItems = List(menuData.size) { index ->
-    val (_, name, _) = menuData[index].split(",")
-    name
-}
-private val menuItemPrices: Map<String, Double> = List(menuData.size) {index ->
-    val (_, name, price) = menuData[index].split(",")
+    .map { it.split(",") }
+
+private val menuItems = menuData.map { (_, name, _) -> name }
+
+private val menuItemPrices = menuData.associate { (_, name, price) ->
     name to price.toDouble()
-}.toMap()
-private val menuItemTypes: Map<String, String> = List(menuData.size) {index ->
-    val (type, name, _) = menuData[index].split(",")
+}
+private val menuItemTypes = menuData.associate { (type, name, _) ->
     name to type
-}.toMap()
+}
 
 fun visitTavern() {
     narrate("$heroName enters $TAVERN_NAME")
     narrate("There are several items for sale:")
     narrate(menuItems.joinToString())
 
-    val patrons: MutableSet<String> = mutableSetOf()
-    val patronGold = mutableMapOf(TAVERN_MASTER to 86.99, heroName to 4.50)
-    while (patrons.size < 5) {
-        val patronName = "${firstNames.random()} ${lastNames.random()}"
-        patrons += patronName
-        patronGold += patronName to 6.00
-    }
+    val patrons: MutableSet<String> = firstNames.shuffled().zip(lastNames.shuffled()) { firstName, lastname ->
+        "$firstName $lastname"
+    }.toMutableSet()
+    val patronGold = mutableMapOf(TAVERN_MASTER to 86.99, heroName to 4.50, *patrons.map {
+        it to 6.00
+    }.toTypedArray())
+
     narrate("$heroName sees several patrons in the tavern:")
     narrate(patrons.joinToString())
+
+    val itemDay = patrons.flatMap { getFavoriteMenuItem(it) }.random()
+    narrate("The item of the day is the $itemDay")
     repeat(3) {
         placeOrder(patrons.random(), menuItems.random(), patronGold)
     }
     displayPatronBalances(patronGold)
+
+    val departingPatrons: List<String> = patrons.filter { patron -> patronGold.getOrDefault(patron, 0.0) < 4.0 }
+    patrons -= departingPatrons
+    patronGold -= departingPatrons
+    departingPatrons.forEach { patron ->
+        narrate("$heroName sees $patron departing the tavern")
+    }
+    narrate("There are still some patrons in the Tavern")
+    narrate(patrons.joinToString())
 }
 
-fun placeOrder(patronName: String, menuItemName: String, patronGold: MutableMap<String, Double>) {
+private fun getFavoriteMenuItem(patron: String): List<String> {
+    return when (patron) {
+        "Alex Ironfoot" -> menuItems.filter { menuItem ->
+            menuItemTypes[menuItem]?.contains("dessert") == true
+        }
+
+        else -> {
+            menuItems.shuffled().take(Random.nextInt(1..2))
+        }
+    }
+}
+
+private fun placeOrder(patronName: String, menuItemName: String, patronGold: MutableMap<String, Double>) {
     val itemPrice = menuItemPrices.getValue(menuItemName)
     narrate("$patronName speaks with $TAVERN_MASTER to place an order")
     if (itemPrice <= patronGold.getOrDefault(patronName, 0.0)) {
-        val action = when(menuItemTypes[menuItemName]) {
+        val action = when (menuItemTypes[menuItemName]) {
             "shandy", "elixir" -> "pours"
             "meal" -> "serves"
             else -> "hands"
@@ -58,8 +82,10 @@ fun placeOrder(patronName: String, menuItemName: String, patronGold: MutableMap<
         narrate("$TAVERN_MASTER says, \"You need more caoin for $itemPrice\"")
     }
 }
+
 private fun displayPatronBalances(patronGold: Map<String, Double>) {
     narrate("$heroName intuitively knows how much money each patron has")
-    patronGold.forEach {patron, balance ->
-    narrate("$patron has ${"%.2f".format(balance)} gold")}
+    patronGold.forEach { patron, balance ->
+        narrate("$patron has ${"%.2f".format(balance)} gold")
+    }
 }
