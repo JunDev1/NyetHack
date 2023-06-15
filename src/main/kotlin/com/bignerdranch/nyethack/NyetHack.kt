5ebcdba7710ab0550f27ec0333a3feba0f2875e6
JunDev1 <86519269+JunDev1@users.noreply.github.com>
2023-06-15 21:29:46 +0300

    Chapter 17

--- src/main/kotlin/com/bignerdranch/nyethack/NyetHack.kt
+++ src/main/kotlin/com/bignerdranch/nyethack/NyetHack.kt
@@ -1,6 +1,8 @@
 package com.bignerdranch.nyethack
 
-lateinit var player : Player
+import kotlin.system.exitProcess
+
+lateinit var player: Player
 fun main() {
     narrate("Welcome to NyetHack")
     val playerName = promptHeroName()
@@ -24,15 +26,17 @@ fun promptHeroName(): String {
 
 object Game {
     private val worldMap = listOf(
-        listOf(TownSquare(), Tavern(),Room("Back Room")),
-        listOf(Room("A Long Corridor"), Room("A Generic Room")),
-        listOf(Room("The Dungeon"))
+        listOf(TownSquare(), Tavern(), Room("Back Room")),
+        listOf(MonsterRoom("A Long Corridor"), Room("A Generic Room")),
+        listOf(MonsterRoom("The Dungeon"))
     )
-    private var currentRoom : Room = worldMap[0][0]
-    private var currentPosition = Coordinate(0,0)
+    private var currentRoom: Room = worldMap[0][0]
+    private var currentPosition = Coordinate(0, 0)
+
     init {
         narrate("Welcome, adventurer")
     }
+
     fun play() {
         while (true) {
             narrate("${player.name} of ${(player.homeTown)}, ${player.title}, is in ${currentRoom.description()}")
@@ -43,6 +47,7 @@ object Game {
             GameInput(readLine()).processCommand()
         }
     }
+
     fun move(direction: Direction) {
         val newPosition = direction.updateCoordinate(currentPosition)
         val newRoom = worldMap.getOrNull(newPosition.y)?.getOrNull(newPosition.x)
@@ -54,11 +59,36 @@ object Game {
             narrate("You can't move ${direction.name}")
         }
     }
-    private class GameInput(arg : String?) {
-        private val input = arg?: ""
-        val command = input.split("")[0]
-        val argument = input.split("").getOrElse(1) {""}
+
+    fun fight() {
+        val monsterRoom = currentRoom as? MonsterRoom
+        val currentMonster = monsterRoom?.monster
+        if (currentMonster == null) {
+            narrate("There's nothing to fight here")
+            return
+        }
+        while (player.healthPoints > 0 && currentMonster.healthPoints > 0) {
+            player.attack(currentMonster)
+            if (currentMonster.healthPoints > 0) {
+                currentMonster.attack(player)
+            }
+            Thread.sleep(1000)
+        }
+        if (player.healthPoints <=0 ) {
+            narrate("You have benn defeated! Thanks for playing")
+            exitProcess(0)
+        } else {
+            narrate("${currentMonster.name} has been defeated")
+            monsterRoom.monster = null
+        }
+    }
+
+    private class GameInput(arg: String?) {
+        private val input = arg ?: ""
+        val command = input.split(" ")[0]
+        val argument = input.split(" ").getOrElse(1) { "" }
         fun processCommand() = when (command.lowercase()) {
+            "fight" -> fight()
             "move" -> {
                 val direction = Direction.values().firstOrNull() {
                     it.name.equals(argument, ignoreCase = true)
--- src/main/kotlin/com/bignerdranch/nyethack/Room.kt
+++ src/main/kotlin/com/bignerdranch/nyethack/Room.kt
@@ -2,11 +2,12 @@ package com.bignerdranch.nyethack
 
 open class Room(val name: String) {
     protected open val status = "Calm"
-    fun description() = "$name (Currently : $status)"
+    open fun description() = "$name (Currently : $status)"
     open fun enterRoom() {
         narrate("There's nothing to do here")
     }
 }
+
 open class TownSquare() : Room("The Town Square") {
     override val status: String
         get() = "Bustling"
@@ -15,7 +16,24 @@ open class TownSquare() : Room("The Town Square") {
         narrate("The villagers rally and cheer as the hero enters")
         ringBell()
     }
+
     private fun ringBell() {
         narrate("The bell tower announces the hero's presence: $bellSound")
+    }
+}
+
+open class MonsterRoom(
+    name: String,
+    var monster: Monster? = Goblin()
+) : Room(name) {
+    override fun description() =
+        super.description() + "(Creature: ${monster?.description ?: "None"})"
+
+    override fun enterRoom() {
+        if (monster == null) {
+            super.enterRoom()
+        } else {
+            narrate("Danger is lurking in this room")
+        }
     }
 }
